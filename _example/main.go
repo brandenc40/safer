3416d94224fe5112a7496ece54f6c89fa65f8ce2
Branden Colen <brandencolen@gmail.com>
2021-08-19 19:15:39 -0500

    fixed example

--- _example/main.go
+++ _example/main.go
@@ -11,18 +11,18 @@ import (
 func main() {
 	client := safer.NewClient()
 
-	//// by mc/mx
-	//s := time.Now()
-	//snapshot, err := client.GetCompanyByMCMX("133655")
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//log.Println(time.Since(s).String())
-	//log.Printf("%+v", snapshot)
+	// by mc/mx
+	s := time.Now()
+	snapshot, err := client.GetCompanyByMCMX("133655")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(time.Since(s).String())
+	log.Printf("%+v", snapshot)
 
 	// by dot
-	s := time.Now()
-	snapshot, err := client.GetCompanyByDOTNumber("1003306")
+	s = time.Now()
+	snapshot, err = client.GetCompanyByDOTNumber("1003306")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -30,14 +30,14 @@ func main() {
 	v, _ := json.Marshal(snapshot)
 	log.Printf(string(v))
 
-	//// search results and grab snapshot from result
-	//res, err := client.SearchCompaniesByName("Schneider")
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//log.Printf("%#v", res[0])
-	//snapshot, err = client.GetCompanyByDOTNumber(res[0].DOTNumber)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+	// search results and grab snapshot from result
+	res, err := client.SearchCompaniesByName("Schneider")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("%#v", res[0])
+	snapshot, err = client.GetCompanyByDOTNumber(res[0].DOTNumber)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
